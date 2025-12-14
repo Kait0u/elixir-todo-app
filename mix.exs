a9e17d9b168a6560eed11641690b86f4ddc34d2a
Kait0u <jjaw.stuff@gmail.com>
2025-12-14 18:32:03 +0100

    Add Jason dependency and escript.

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Todo.MixProject do
       app: :todo,
       version: "0.1.0",
       elixir: "~> 1.19",
+      escript: escript(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -19,11 +20,16 @@ defmodule Todo.MixProject do
     ]
   end
 
+  defp escript do
+    [
+      main_module: TodoApp.CLI
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:jason, "~> 1.4"}
     ]
   end
 end
